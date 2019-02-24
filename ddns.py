3c66ca5a5a29c2eac13a98e658cd61e9715f03e9
erick <erick.duran@cetys.edu.mx>
2019-02-23 16:37:04 -0800

    v2

--- ddns.py
+++ ddns.py
@@ -1,17 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import requests
 import logging
 import time
+from urllib2 import urlopen
 
 logging.basicConfig(filename='ddns.log', level=logging.DEBUG, filemode='a', format='%(asctime)s - %(message)s')
 
 username = 'username'
 password = 'password'
 hostname = 'your-domain.com'
-url = 'https://' + username + ':' + password + '@domains.google.com/nic/update?hostname=' + hostname
+old_ip = ''
 
 while True:
-	response = requests.post(url)
-	output = response.content.decode('utf-8')
-	logging.debug('Response from DDNS update: '+ output)
-	time.sleep(300)
+	my_ip = urlopen('https://domains.google.com/checkip').read() 
+	if my_ip != old_ip:
+		url = 'https://' + username + ':' + password + '@domains.google.com/nic/update?hostname=' + hostname
+			response = requests.post(url)
+			output = response.content.decode('utf-8')
+			if 'good' in output:
+				old_ip = my_ip
+			logging.debug('Response from DDNS update: '+ output)
+	time.sleep(10)
