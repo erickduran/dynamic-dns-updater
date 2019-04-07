60199982d99e81a5586c51d06dfff837d912f915
erick <erick.duran@cetys.edu.mx>
2019-04-07 14:15:16 -0700

    v3

--- ddns.py
+++ ddns.py
@@ -1,23 +1,29 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import requests
 import logging
 import time
-from urllib2 import urlopen
+from urllib.request import urlopen
 
 logging.basicConfig(filename='ddns.log', level=logging.DEBUG, filemode='a', format='%(asctime)s - %(message)s')
 
 username = 'username'
 password = 'password'
-hostname = 'your-domain.com'
+hostname = 'example.com'
 old_ip = ''
 
 while True:
-	my_ip = urlopen('https://domains.google.com/checkip').read() 
-	if my_ip != old_ip:
-		url = 'https://' + username + ':' + password + '@domains.google.com/nic/update?hostname=' + hostname
+	try:
+		my_ip = urlopen('https://domains.google.com/checkip').read() 
+	except:
+		logging.debug('CATCHED AN ERROR... RETRYING IN 10 SECONDS')
+		time.sleep(10)
+	else:
+		if my_ip != old_ip:
+			url = 'https://{}:{}@domains.google.com/nic/update?hostname={}'.format(username, password, hostname)
 			response = requests.post(url)
 			output = response.content.decode('utf-8')
-			if 'good' in output:
+			if 'good' in output or 'nochg' in output:
 				old_ip = my_ip
+			logging.debug('-- OUTPUT FOR UPDATE: '+ hostname +' --')
 			logging.debug('Response from DDNS update: '+ output)
-	time.sleep(10)
+		time.sleep(10)
